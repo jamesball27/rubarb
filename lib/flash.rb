d945e0b7ea5dcacc2048e58929d12077576c9447
jamesball27 <jwrb27@gmail.com>
2017-05-02 11:08:13 -0400

    Refactor Flash

--- lib/flash.rb
+++ lib/flash.rb
@@ -4,35 +4,33 @@ class Flash
 
   def initialize(req)
     cookie = req.cookies["_rails_lite_app_flash"]
-
-    if cookie
-      @flash_now_data = JSON.parse(cookie)
-    else
-      @flash_now_data = {}
-    end
-
+    
+    @flash_now_data = cookie ? JSON.parse(cookie) : {}
     @flash_data = {}
   end
 
-  def [](key)
-    @flash_now_data[key.to_s]
-  end
-
-  def []=(key, val)
-    @flash_data[key] = val
-    @flash_now_data[key] = val
-  end
-
   def now
-    @flash_now_data
+    flash_now_data
   end
 
   def store_flash(res)
     res.set_cookie(
-      "_rails_lite_app_flash", {
-        path: "/",
-        value: @flash_data.to_json
-      })
+      "_rails_lite_app_flash",
+      { path: "/", value: flash_data.to_json }
+    )
   end
-  
+
+  private
+
+  attr_reader :flash_data, :flash_now_data
+
+  def [](key)
+    flash_now_data[key.to_s]
+  end
+
+  def []=(key, val)
+    flash_data[key] = val
+    flash_now_data[key] = val
+  end
+
 end
